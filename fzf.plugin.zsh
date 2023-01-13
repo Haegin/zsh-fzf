046617a4b63ce4c9a2c3b92f32d2e9452862a67f
Harry Porter-Mills <harry@portermills.net>
2023-01-13 15:50:06 -0500

    Update key-binding.sh path for Ubuntu

--- fzf.plugin.zsh
+++ fzf.plugin.zsh
@@ -3,4 +3,4 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
-source /usr/local/opt/fzf/shell/key-bindings.zsh
+source /usr/share/doc/fzf/examples/key-bindings.zsh
