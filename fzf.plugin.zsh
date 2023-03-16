aec9859008dc27efda259350e5bdb2cb71481318
Harry Porter-Mills <harry@portermills.net>
2023-03-15 23:39:13 -0400

    Add shoddy mac support

--- fzf.plugin.zsh
+++ fzf.plugin.zsh
@@ -1,6 +1,12 @@
 export FZF_DEFAULT_COMMAND='rg --files --hidden --smart-case --follow --glob "!.git/*"'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+if [ -z "${HOMEBREW_PREFIX+x}" ]; then
+  [[ $- == *i* ]] && source "${HOMEBREW_PREFIX}/opt/fzf/shell/completion.zsh" 2> /dev/null
 
-source /usr/share/doc/fzf/examples/key-bindings.zsh
+  source "${HOMEBREW_PREFIX}/opt/fzf/shell/key-bindings.zsh"
+else
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
+fi
